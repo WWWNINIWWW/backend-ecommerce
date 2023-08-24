ce4c7c5bd4faf23fe34641aed732106f22a6f4cb
WWWNINIWWW <nicolasguedes1@gmail.com>
2023-08-23 21:32:00 -0300

    refactor(users): add parameters

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    user_id = models.IntegerField(unique=True,null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
