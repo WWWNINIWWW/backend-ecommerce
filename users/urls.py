d446ffbcd60e088faff1a344c101ba6c2abbaf8e
WWWNINIWWW <nicolasguedes1@gmail.com>
2023-09-06 11:26:18 -0300

    feat(cart): initial routes

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from users.views import UserListAndCreate,UserDetailChangeAndDelete
+from users.views import UserListAndCreate,UserDetailChangeAndDelete, CartList,CartDetailChangeAndDelete
 
 
 urlpatterns = [
     path('',UserListAndCreate.as_view()),
     path('<int:user_id>/',UserDetailChangeAndDelete.as_view()),
+    path('cart/', CartList.as_view()),
+    path('cart/<int:user_id>/',CartDetailChangeAndDelete.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
-from users.models import User
-from users.serializers import UserSerializer
-from rest_framework import generics
+from users.models import User, Cart
+from users.serializers import UserSerializer, CartSerializer
+from rest_framework import generics,status
 from django.shortcuts import get_object_or_404
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
 from products.models import Products
 from orders.models import Order
+from rest_framework.response import Response
 
 class UserListAndCreate(generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -19,6 +20,18 @@ class UserDetailChangeAndDelete(generics.RetrieveUpdateDestroyAPIView):
         user = get_object_or_404(User,user_id=user_id)
         return user
     
+class CartList(generics.ListAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    
+class CartDetailChangeAndDelete(generics.RetrieveAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    def get_object(self):
+        user_id = self.kwargs['user_id']
+        cart = get_object_or_404(Cart,user_id=user_id)
+        return cart
+    
 @receiver(pre_delete, sender=User)
 def before_delete_user(sender, instance, **kwargs):
     try:
@@ -31,6 +44,13 @@ def before_delete_user(sender, instance, **kwargs):
         for order in orders:
             if order.seller_id == user_id or order.consumer_id == user_id:
                 order.delete()
-        return product,order
+        cart = Cart.objects.filter(user_id=user_id).first().delete()
+        return product,order,cart
     except:
         pass
+
+@receiver(post_save, sender=User)
+def after_save_user(sender, instance, created, **kwargs):
+    if created:
+        cart = Cart.objects.create(user_id=instance.user_id)
+        return cart
